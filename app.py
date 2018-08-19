1186a85dd6453070f54ba54b0777983c1fc52bec
Vaali <pamarthi.vinay@gmail.com>
2018-08-19 16:33:07 -0400

    dynamics changes

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import json
 #import db_utility
 import loggingmodule
 import argparse
+import os
+import glob
 
 app = Flask(__name__)
 params = {}
@@ -24,14 +26,34 @@ tasks = [
 
 #genres_predictor = GenrePredictor('models_for_prediction/1st_level/', '9353', 4)
 #style_predictor = GenrePredictor('models_for_prediction/2nd_level/blues/', '8231', 13)
-
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
+def get_genresCount():
+    filename = os.path.join(params['path'],'genres.txt')
+    if(os.path.exists(filename)):
+        fo = open(filename, "rw+")
+        genreCount = len(fo.readlines())
+    else:
+        genreCount = -1
+    print 'Returning :'+str(genreCount)
+    return genreCount
 
+def get_predictorname():
+    list_meta_files = [os.path.join(params['path'], f) for f in os.listdir(params['path']) if f.endswith(".meta")]
+    #files = os.system('find '+params['path']+' -name "[0-9]*.meta" ')
+    predict_name = ''
+    if(len(list_meta_files) > 0):
+        predict_name = os.path.splitext(os.path.basename(list_meta_files[0]))[0]
+    else:
+        print 'cant find meta file'
+    print predict_name
+    return predict_name
+    
 
 @app.route('/gramusik/v1/predict/<string:youtube_id>', methods=['GET'])
 def find_genres(youtube_id):
+    global genres_predictor
     is_force_download = False
     if 'refresh' in request.args:
         is_force_download = request.args['refresh']
@@ -91,25 +113,37 @@ def not_found(error):
 
 
 if __name__ == '__main__':
+    global genres_predictor
     logger_predict = loggingmodule.initialize_logger('predictor','genre_predictor.log')
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-path',"--path", help="path to the data model")
-    #
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-port',"--port", help="port number of the application")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-path',"--path", help="path to the data model")
 
     #parser = argparse.ArgumentParser()
-    #parser.add_argument("--name", help="name of the predictor")
+    parser.add_argument('-port',"--port", help="port number of the application")
+
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('-n',"--name", help="name of the predictor")
     
     #parser = argparse.ArgumentParser()
-    #parser.add_argument("--genrescount", help="number of genres")
+    #parser.add_argument('-g',"--genrescount", help="number of genres")
 
-    # args = parser.parse_args()
-    # params['path'] = args.path
-    # params['port'] = int(args.port)
+    args = parser.parse_args()
+    if('path' in args and args.path != ''):
+        params['path'] = args.path
+    else:
+        params['path'] = 'models_for_prediction/1st_level/'
+    if('port' in args and args.port != ''):
+        params['port'] = int(args.port)
+    else:
+        params['port'] = 8970
     #params['name'] = args.name
     #params['genrescount'] = int(args.genrescount)
-    #genres_predictor = GenrePredictor(params['path'], params['name'], params['genrescount'])
-    genres_predictor = GenrePredictor('models_for_prediction/1st_level/', '9353', 4)
+    
 
-    app.run(host='0.0.0.0', port=8978)
+    genreCount = get_genresCount()
+    if(genreCount == -1):
+        print 'genres.txt not found'
+    predict_name = get_predictorname();
+    genres_predictor = GenrePredictor(params['path'], predict_name, genreCount)
+
+    app.run(host='0.0.0.0', port=params['port'])
